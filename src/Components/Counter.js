32e08e91e38f67aed1c895469535ba0a0066c523
saju575 <sakilahmedsaju575@gmail.com>
2023-02-19 14:48:48 +0600

    1.2 section complete

---
+++ src/Components/Counter.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+
+const Counter = () => {
+    const [count, setCount] = useState(0)
+
+    const increment = () => {
+        setCount((prevCount)=>prevCount+1)
+    }
+    const decrement = () => {
+        setCount((prevCount)=>prevCount-1)
+    }
+    
+  return (
+    
+      <div className="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow"
+                >
+          <div className="text-2xl font-semibold" id="counter">{count}</div>
+                    <div className="flex space-x-3">
+                        <button
+                            className="bg-indigo-400 text-white px-3 py-2 rounded shadow"
+                  id="increment"
+                    onClick={increment}
+                        >
+                            Increment
+                        </button>
+                        <button
+                            className="bg-red-400 text-white px-3 py-2 rounded shadow"
+                  id="decrement"
+                    onClick={decrement}
+                        >
+                            Decrement
+                        </button>
+                    </div>
+                </div>
+    
+  )
+}
+
+export default Counter
