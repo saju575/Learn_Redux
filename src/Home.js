85ce9f75adb2a172ada3e066c88f8be76edd4a6f
saju575 <sakilahmedsaju575@gmail.com>
2023-02-21 23:51:02 +0600

    2.1 and 2.2 complete

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,20 @@
-import React, { useState } from 'react'
-import Counter from './Components/Counter'
-import Stats from './Components/Stats'
+import React, { useState } from "react";
+import Counter from "./Components/Counter";
+import Stats from "./Components/Stats";
 
 const initialState = [
   {
     id: 1,
-    count:0
+    count: 0,
   },
   {
     id: 2,
-    count:0
-  }
-]
+    count: 0,
+  },
+];
 
 const Home = () => {
-
-  const [state, setState] = useState(initialState)
+  const [state, setState] = useState(initialState);
 
   //increment
 
@@ -24,16 +23,16 @@ const Home = () => {
       if (c.id === id) {
         return {
           ...c,
-          count:c.count+1
-        }
+          count: c.count + 1,
+        };
       }
       return {
-        ...c
-      }
-    })
-    setState(updatedCounter)
-  }
-  
+        ...c,
+      };
+    });
+    setState(updatedCounter);
+  };
+
   //decrement
 
   const decrement = (id) => {
@@ -41,46 +40,43 @@ const Home = () => {
       if (c.id === id) {
         return {
           ...c,
-          count:c.count-1
-        }
+          count: c.count - 1,
+        };
       }
       return {
-        ...c
-      }
-    })
-    setState(updatedCounter)
-  }
-  
+        ...c,
+      };
+    });
+    setState(updatedCounter);
+  };
+
   //total count
 
   const totalCount = () => {
-    return state.reduce((total,counter)=>total+counter.count,0)
-  }
+    return state.reduce((total, counter) => total + counter.count, 0);
+  };
 
   return (
     <div className="w-screen h-screen p-10 bg-gray-100 text-slate-700">
-            
-            <h1 className="max-w-md mx-auto text-center text-2xl font-bold">
-                Simple Counter Application
-            </h1>
+      <h1 className="max-w-md mx-auto text-center text-2xl font-bold">
+        Simple Counter Application
+      </h1>
 
-            
-            <div className="max-w-md mx-auto mt-10 space-y-5">
-       
-        {
-          state.map(count => <Counter
+      <div className="max-w-md mx-auto mt-10 space-y-5">
+        {state.map((count) => (
+          <Counter
             key={count.id}
             id={count.id}
             count={count.count}
             increment={increment}
             decrement={decrement}
-          />)
-        }
-        
-        <Stats count={totalCount()}/>
-            </div>
-        </div>
-  )
-}
+          />
+        ))}
 
-export default Home
+        <Stats count={totalCount()} />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/Redux/Counter/actions.js
@@ -0,0 +1,15 @@
+import { INCREMENT, DECREMENT } from "./actionsTypes";
+
+export const increment = (value) => {
+  return {
+    type: INCREMENT,
+    payload: value,
+  };
+};
+
+export const decrement = (value) => {
+  return {
+    type: DECREMENT,
+    payload: value,
+  };
+};
